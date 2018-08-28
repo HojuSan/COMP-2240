5d394ba137914648e4ae8e63d64ea766bef42381
juyong kim <Juyong.Kim@uon.edu.au>
2018-08-28 14:03:45 +1000

    starting point

---
+++ A1.java
@@ -0,0 +1,37 @@
+//c3244203 Juyong Kim
+
+//First come first serve
+class FCFS
+{
+    public static doStuff()
+    {
+        // do stuff
+    }
+}
+
+//Round Robin
+class RR
+{
+    public static doStuff()
+    {
+        // do stuff
+    }
+}
+
+//Feedback constant
+class FB
+{
+    public static doStuff()
+    {
+        // do stuff
+    }
+}
+
+//Narrow round robin
+class NRR
+{
+    public static doStuff()
+    {
+        // do stuff
+    }
+}
