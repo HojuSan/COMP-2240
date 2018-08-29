8b9b89df770defa934e641ef3efcefb1996d0e99
juyong kim <Juyong.Kim@uon.edu.au>
2018-08-29 14:13:43 +1000

    created a struct

---
+++ Process.java
@@ -0,0 +1,18 @@
+class Process {
+       private String ID;
+       private int Arrival;
+
+   // constructor
+   public Employee(String name, int code) {
+      this.ID = name;
+      this.Arrival = code;
+   }
+
+       // getter
+       public String getName() { return name; }
+       public int getCode() { return code; }
+       // setter
+
+       public void setName(String name) { this.name = name; }
+       public void setCode(int code) { this.code = code; }
+    }
