57311f0f7ce64e69814c1b9dcd5668cccddc60df
juyong kim <Juyong.Kim@uon.edu.au>
2018-08-30 20:35:46 +1000

    now

--- FCFS.java
+++ FCFS.java
@@ -6,107 +6,112 @@ Date:       09/08/18
 Description: First come first serve
 */
 
-
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.List;
+import java.lang.System;
+import java.util.PriorityQueue;
 
 public class FCFS
 {
-    //variable for amount of process there will be initially
-    private Queue<Process> q;
-    final private int MAX = 5;
-    private Process[] process;
-	private int total = 0;
-    private int dis = 0;
+    //private ArrayQueue<Process> que,done;
+    private PriorityQueue<Process> que;// = new PriorityQueue<>();
+    private PriorityQueue<Process> done;// = new PriorityQueue<>();
+    private Process[] pro;// = new Process();
+	private int total1;
+    private int total2;
 
     public FCFS()
     {
-        q = new LinkedList();
-        process = new Process[MAX]; 
-        total = 0;
+        que = new PriorityQueue<Process>();
+        done = new PriorityQueue<Process>();
+        pro = new Process[0]; 
+        total2 = 0;
+        total1 = 0;
     }
-
+/*
+        //Setting the process into a list
+        public void addProcess(String id, int arrival, int execute)
+        {
+            if (check(id) == 0)
+            {
+                setProcess(pro[total1],id,arrival,execute);
+                que.add(pro[total1]);
+                total1++;
+    
+                if(total == process.length)
+                {
+                    resize();
+                }
+            }
+            
+            else if (check(id) == 1)
+            {
+                System.out.println("ID already exists");
+            }
+        }
+    
+        public int check(String id)
+        {
+            if (total1 > 0)
+            {
+                for(int i = 0; i < total1; i++)
+                {
+                    if (process[i].getId() == id)
+                    {
+                        return 1;
+                    }
+                }
+            }
+    
+            return 0;
+        }
+    
+        public void setProcess(Process process, String id, int a, int e)
+        {
+            process.setId(id);
+            process.setArrival(a);
+            process.setExecute(e);
+        }
+    
+        public void resize()
+        {
+            reProcess = new Process[process.length+1];
+    
+            for (int i = 0; i < process.length; i++)
+            {
+                reProcess[i] = process[i];
+            }
+    
+            process = reProcess;
+        }
+    }
+*/
+/*
     public void doStuff()
     {
         int counter = 0;
-
+        int num = total1;
         System.out.println("FCFS:");
 
-        for (int i = 0; i <= total; i++)
+        for (int i = 0; i <= num; i++)
         {
             counter += c3244203A1.getDis();
 
-            System.out.println("T" + counter + ":" + process[i].getId());
+            System.out.println("T" + counter + ":" + pro[i].getId());
+            counter += pro[i].getExecute();
+
+            for (int i = 0; i < total1; i++)
+            {
+                pro[i].setTr(pro[i].getTr() + counter);
+            }
+
+            done.add(pro[i]);
+            total2++;
+            que.push(pro[i]);
+            total1--;
         }
 
         System.out.println("Process Turnaround Time Waiting Time");
         for (int i = 0; i <= total; i++)
         {
-            System.out.println(process[i].getId() + "  " + process[i].getTr() + "      " + process[i].getWt());
-        }
+            System.out.println(pro[i].getId() + "  " + pro[i].getTr() + "      " + pro[i].getWt());
+        }*/
     }
-
-    //Setting the process into a list
-    public void addProcess(String id, int arrival, int execute)
-    {
-        if (dis == 0)
-        {
-            dis = c3244203A1.getDis();
-        } 
-
-        if (check(id) == 0)
-        {
-            process[total] = new Process();
-            setProcess(process[total],id,arrival,execute);
-            q.add(process[total]);
-            total++;
-
-            if(total == process.length)
-            {
-                resize();
-            }
-        }
-        
-        else if (check(id) == 1)
-        {
-            System.out.println("ID already exists");
-        }
-    }
-
-    public int check(String id)
-    {
-        if (total > 0)
-        {
-            for(int i = 0; i < total; i++)
-            {
-                if (process[i].getId() == id)
-                {
-                    return 1;
-                }
-            }
-        }
-
-        return 0;
-    }
-
-    public void setProcess(Process process, String id, int a, int e)
-    {
-        process.setId(id);
-        process.setArrival(a);
-        process.setExecute(e);
-    }
-
-    public void resize()
-    {
-        Process [] reProcess;
-        reProcess = new Process[process.length+1];
-
-        for (int i = 0; i < process.length; i++)
-        {
-            reProcess[i] = process[i];
-        }
-
-        process = reProcess;
-    }
-}
--- c3244203A1.java
+++ c3244203A1.java
@@ -12,33 +12,40 @@ public class c3244203A1
 {
 
     static Scanner input = new Scanner(System.in);    
-    static FCFS fcfs;
-    //static RR rr;
-    //static FB fb;
-    //static NRR nrr;
-    public static int dis = 0;
+    private FCFS fcfs;
     
 
     public static void main (String [] args)
     {
+        FCFS fcfs = new FCFS();
+        //static RR rr;
+        //static FB fb;
+        //static NRR nrr;
+        int dis = 1;
 
+        System.out.println("working");
+ //       fcfs.addProcess("p1",0,10);
+ //       fcfs.addProcess("p2",0,1);
+ //       fcfs.addProcess("p3",0,2);
+//      fcfs.addProcess("p4",0,1);
+ //       fcfs.addProcess("p5",0,5);
+/*
         //setting dispatcher 
         System.out.println("Dispatcher runtime?");
-        dis = input.nextInt();
+        dis = Integer.parseInt(input.nextLine());
 
         //setting amount of process in usage
         System.out.println("How many process do you wish to evaluate");
-        int total = input.nextInt();
+        int total = Integer.parseInt(input.nextLine());
 
         for(int i = 0; i <= total; i++)
         {
             addProcess();
-        }
-
-        
-
-    }     
-
+        }      
+*/
+    }
+ 
+/*
     //Adds new process 
     public static void addProcess()
 	{
@@ -46,18 +53,18 @@ public class c3244203A1
 		String id = input.nextLine();
 
         System.out.println("What is the execution time:");
-		int execute = input.nextInt();
+		int execute = Integer.parseInt(input.nextLine());
 
         System.out.println("What is the arrival time:");
-		int arrival = input.nextInt();
+		int arrival = Integer.parseInt(input.nextLine());
 		
         fcfs.addProcess(id,arrival,execute);   
         //rr.addProcess(id,arrival,execute);   
         //fb.addProcess(id,arrival,execute);   
 		//nrr.addProcess(id,arrival,execute);   		
 	}
-
-    public static int getDis()
+*/
+    public int getDis()
     {
         return dis;
     }
